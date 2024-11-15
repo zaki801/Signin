f37ba5bb64b54f1829e0a809a0dfed0441a64d56
zaki801 <me3634595@gmail.com>
2024-11-15 14:53:05 +0200

    Add files via upload

---
+++ index.js
@@ -0,0 +1,13 @@
+
+// sign in
+
+document.getElementById('signupForm').addEventListener('submit', function(event) {
+    event.preventDefault();
+    alert("Sign-up form submitted!");
+});
+// log in
+
+document.getElementById('loginForm').addEventListener('submit', function(event) {
+    event.preventDefault();
+    alert("Login form submitted!");
+});
